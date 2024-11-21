5e71840733dd98eaa78f0f5981930f889ff49d10
jessicabalonecker <00001072907513sp@al.educacao.sp.gov.br>
2024-11-21 14:36:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Educação Física',
+    'Qual é a principal função do aquecimento antes de um exercício?',
+    'A principal função do aquecimento é preparar os músculos e o sistema cardiovascular para a atividade física, reduzindo o risco de lesões.'
+)
+
+criaCartao(
+    'Arte',
+    'O que caracteriza o movimento impressionista?',
+    'O impressionismo é caracterizado pelo uso de pinceladas soltas e a captura da luz e da atmosfera em pinturas ao ar livre.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é DNA?',
+    'DNA (ácido desoxirribonucleico) é a molécula que carrega a informação genética em todos os organismos vivos.'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem é conhecido como o pai da filosofia ocidental?',
+    'Sócrates é conhecido como o pai da filosofia ocidental.'
+)
